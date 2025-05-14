f51a86f529992d13c275d538f0b4682db0dc2641
Raditya Jagadhita <radityaraditya472@gmail.com>
2025-05-14 11:34:06 +0700

    membuat abstract BUKU dan menghapus service, controller, dan repository buku

--- src/main/java/kelompok7/library_school/model/Buku.java
+++ src/main/java/kelompok7/library_school/model/Buku.java
@@ -4,11 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "buku")
-public class Buku {
+public abstract class Buku {
     @Id
     @GeneratedValue
     private Long id;
--- src/main/java/kelompok7/library_school/repository/BukuRepository.java
+++
@@ -1,9 +0,0 @@
-package kelompok7.library_school.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kelompok7.library_school.model.Buku;
-
-
-public interface BukuRepository extends JpaRepository<Buku, Long> {
-}
