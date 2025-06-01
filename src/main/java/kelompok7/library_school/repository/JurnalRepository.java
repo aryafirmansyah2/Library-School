f454650a8ac4a68ae0847d6e2a21b744ac4e46b6
valtheen <nauvalfatheen@gmail.com>
2025-06-01 17:07:53 +0700

    Tambah search by jurnal

--- src/main/java/kelompok7/library_school/repository/JurnalRepository.java
+++ src/main/java/kelompok7/library_school/repository/JurnalRepository.java
@@ -1,7 +1,10 @@
 package kelompok7.library_school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 import kelompok7.library_school.model.Jurnal;
 
-public interface JurnalRepository extends JpaRepository<Jurnal, Long> {}
+public interface JurnalRepository extends JpaRepository<Jurnal, Long> {
+    List<Jurnal> findByJudulContainingIgnoreCase(String judul);
+}
